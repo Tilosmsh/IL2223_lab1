5ebd1d0d681a380d4376e55ddce5b6ee97d46fe4
Chenzhou <171994636@qq.com>
2022-11-18 09:41:14 +0100

    Update titanic-training-pipeline.py

--- titanic-training-pipeline.py
+++ titanic-training-pipeline.py
@@ -32,6 +32,69 @@ def g():
     # fs is a reference to the Hopsworks Feature Store
     fs = project.get_feature_store()
 
+    # The feature view is the input set of features for your model. The features can come from different feature groups.
+    # You can select features from different feature groups and join them together to create a feature view
+    try:
+        feature_view = fs.get_feature_view(name="titanic_modal", version=1)
+    except:
+        iris_fg = fs.get_feature_group(name="titanic_modal", version=1)
+        query = iris_fg.select_all()
+        feature_view = fs.create_feature_view(name="titanic_modal",
+                                              version=1,
+                                              description="Read from titanic dataset",
+                                              labels=["survived"],
+                                              query=query)
+
+    # You can read training data, randomly split into train/test sets of features (X) and labels (y)
+    X_train, X_test, y_train, y_test = feature_view.train_test_split(0.2)
+
+    # Train our model with the Scikit-learn K-nearest-neighbors algorithm using our features (X_train) and labels (y_train)
+    # XGBoost for titanic
+    model = KNeighborsClassifier(n_neighbors=2)
+    model.fit(X_train, y_train.values.ravel())
+
+    # Evaluate model performance using the features from the test set (X_test)
+    y_pred = model.predict(X_test)
+
+    # Compare predictions (y_pred) with the labels in the test set (y_test)
+    metrics = classification_report(y_test, y_pred, output_dict=True)
+    results = confusion_matrix(y_test, y_pred)
+
+    # Create the confusion matrix as a figure, we will later store it as a PNG image file
+    df_cm = pd.DataFrame(results, ['True survived', 'True dead'],
+                         ['Pred survived', 'Pred dead'])
+    cm = sns.heatmap(df_cm, annot=True)
+    fig = cm.get_figure()
+
+    # We will now upload our model to the Hopsworks Model Registry. First get an object for the model registry.
+    mr = project.get_model_registry()
+
+    # The contents of the 'iris_model' directory will be saved to the model registry. Create the dir, first.
+    model_dir = "titanic_modal"
+    if os.path.isdir(model_dir) == False:
+        os.mkdir(model_dir)
+
+    # Save both our model and the confusion matrix to 'model_dir', whose contents will be uploaded to the model registry
+    joblib.dump(model, model_dir + "/titanic_modal.pkl")
+    fig.savefig(model_dir + "/confusion_matrix.png")
+
+    # Specify the schema of the model's input/output using the features (X_train) and labels (y_train)
+    input_schema = Schema(X_train)
+    output_schema = Schema(y_train)
+    model_schema = ModelSchema(input_schema, output_schema)
+
+    # Create an entry in the model registry that includes the model's name, desc, metrics
+    titanic_model = mr.python.create_model(
+        name="titanic_modal",
+        metrics={"accuracy": metrics['accuracy']},
+        model_schema=model_schema,
+        description="Titanic Predictor"
+    )
+
+    # Upload the model to the model registry, including all files in 'model_dir'
+    titanic_model.save(model_dir)
+
+"""
     # My code
     X_train = train.drop('Survived', axis=1)
     y_train = train['Survived']
@@ -51,7 +114,7 @@ def g():
                                    'Standard Deviation': [scores_rf.std()]})
     scores_results
 
-
+"""
 '''
     # The feature view is the input set of features for your model. The features can come from different feature groups.    
     # You can select features from different feature groups and join them together to create a feature view
